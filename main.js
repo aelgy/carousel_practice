8b86d3b655cb5fa85b62856f05a5da6ecfeed96c
aelgy <aelgy333@gmail.com>
2024-04-21 16:15:13 +0800

    obtimize javascript

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ function showSlides(n) {
   let slides = document.querySelectorAll('.slide')
   if (n > slides.length) {
     slideIndex = 1;
-  }
+  };
   if (n < 1) {
     slideIndex = slides.length;
-  }
+  };
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"
   }
